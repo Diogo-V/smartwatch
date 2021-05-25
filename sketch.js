1d72b73724a3e8d1ff1e8ab55d6f109a70c1e75f
Eduardo C <eduardo.a.claudino@gmail.com>
2021-05-25 11:13:59 +0100

    Changed keyboard image

--- sketch.js
+++ sketch.js
@@ -74,7 +74,7 @@ function preload() {
   rightArrow = loadImage("data/right.png");
 
   //Loads custom keyboards
-  img_keyboard = loadImage("./keyboards/Versao2.png");
+  img_keyboard = loadImage("./keyboards/Versao4.png");
 
   swipe_left = loadImage("./data/swipe-left.png");
 }
